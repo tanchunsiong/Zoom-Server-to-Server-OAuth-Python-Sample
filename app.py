697d8534dc84b12998811c752e25ae35b4b340b9
Tan Chun Siong <dreamtcs@hotmail.com>
2023-09-21 13:40:12 +0800

    added redirect for oauth

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, jsonify, request
 from S2SOAuth import get_access_token
 from webhook import handle_request
+from redirectforoauth import handle_redirect_url_data_request
+
+
 import requests
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file, this will try to load these values from your .env file
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -45,6 +54,26 @@ def handle_get_webhook():
     
     # For example, return a response for GET requests
     return 'This is a GET request to webhook.py', 200
+
+
+# Define a route to handle POST requests
+@app.route('/redirecturlforoauth', methods=['GET'])
+def handle_get_oauth():
+    oauth_client_secret = os.getenv("OAUTH_CLIENT_SECRET")
+    oauth_client_id = os.getenv("OAUTH_CLIENT_ID")
+
+    # Get the value of the 'code' query string parameter
+    code = request.args.get('code')
+    
+    # You can handle POST requests here
+    # The request data can be accessed using request.json or request.form
+    # Perform your webhook processing logic
+    response=handle_redirect_url_data_request("redirecturlforoauth",oauth_client_id,oauth_client_secret,code)
+    if response:
+        return response
+    else:
+        print('Failed to retrieve an access token.')   
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ redirectforoauth.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import base64
+import requests
+import urllib.parse  # Import the urllib module for URL encoding
+
+def handle_redirect_url_data_request(path, oauth_client_id, oauth_client_secret, code):
+    print ("handle_redirect_url_data_request")
+    url = f"https://zoom.us/oauth/token"
+    redirect_uri=f"https://python.asdc.cc/{path}"
+    print (redirect_uri)
+    
+    # Encode the client ID and client secret
+    credentials = f"{oauth_client_id}:{oauth_client_secret}"
+    credentials_encoded = base64.b64encode(credentials.encode()).decode('utf-8')
+
+    headers = {
+        'Authorization': f'Basic {credentials_encoded}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+       
+    }
+    print (credentials_encoded)
+
+    data = {
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri,
+        'code': code
+    }
+    # Encode the data dictionary as x-www-form-urlencoded
+    data_encoded = urllib.parse.urlencode(data).encode('utf-8')
+
+
+    response = requests.post(url, data=data_encoded, headers=headers)
+    # Check the HTTP status code before parsing as JSON
+    if response.status_code == 200:
+       print ("response 200")
+       response_json = response.json()
+       print(response_json)
+       return response_json, 200  # Return JSON response with 200 status code
+    else:
+        
+        # Handle the case where the response has an error status code
+        return "Error: " + str(response.status_code), response.status_code
+
+
+# Example usage:
+# handle_redirect_url_data_request('/token', 'YourClientID', 'YourClientSecret', 'sm9Vh2LjAcF6L5oFn_jRm6wYSgkouUpTQ')
